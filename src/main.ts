f47b46c2570e244026e80a42f03f363ff6ac53d4
Purheeart Gharoro <pgharoro@seamfix.com>
2025-02-07 12:36:47 +0100

    update port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(8000);
-  console.log(`Application is running on port: 8000`);
+  await app.listen(process.env.PORT ?? 8000);
+  console.log(`Application is running on port: ${process.env.PORT}`);
 }
 bootstrap();
